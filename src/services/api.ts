363954fbe733f5634e230f267dd562aaa9d9895f
MikaelReis <mikaelreis08@gmail.com>
2025-05-12 11:48:34 -0300

    configurando integrações

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: '---',
+    baseUrl: 'http://localhost:8000/api/',
     prepareHeaders: (headers, { endpoint }) => {
       const token = localStorage.getItem('access_token')
       if (token && endpoint !== 'register') {
